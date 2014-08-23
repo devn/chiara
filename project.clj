3ac067a47084e6abfd9cafa250ab3180f9832e9e
Mike Innes <mike.j.innes@gmail.com>
2014-08-23 14:29:24 +0100

    fix url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject chiara "0.2.0-SNAPSHOT"
   :description "Better S-Expressions"
-  :url "https://github.com/one-more-minute/clarity/"
+  :url "https://github.com/one-more-minute/chiara/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
